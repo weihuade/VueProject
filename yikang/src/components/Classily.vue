87d334692a218d6073856246adc3c6cb06be9ef4
it_call <1486522507@qq.com>
2019-01-09 22:42:04 +0800

    steven

---
+++ yikang/src/components/Classily.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+      <mt-search v-model="value" cancel-text placeholder="搜索疾病，症状或药品"></mt-search>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            value:'',
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+body{
+    margin: 0;
+    padding: 0;
+}
+.search{
+    width: 100%;
+    .mint-searchbar{
+        background-color: #58bc58;
+    }
+}
+</style>
--- yikang/src/router/index.js
+++ yikang/src/router/index.js
@@ -3,8 +3,10 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 import Home from '@/components/Home';
+import Classily from '@/components/Classily';
 export default new Router({
   routes: [
-    {name:'Home',path:'/home',component:Home}
+    {name:'Home',path:'/home',component:Home},
+    {name:'Classily',path:'/classily',component:Classily},
   ]
 })
