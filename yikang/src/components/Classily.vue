2a7f2611ee2181e4736f933c50f82ecd61beb8f7
it_call <1486522507@qq.com>
2019-01-10 23:06:53 +0800

    steven 分类的样式

--- yikang/src/components/Classily.vue
+++ yikang/src/components/Classily.vue
@@ -1,29 +1,99 @@
 <template>
-  <div class="search">
-      <mt-search v-model="value" cancel-text placeholder="搜索疾病，症状或药品"></mt-search>
-  </div>
+    <div>
+        <div class="search">
+        <mt-search v-model="value" cancel-text placeholder="搜索疾病，症状或药品"></mt-search>
+        </div>
+        <div class="catesNav">
+            <div class="cate-list">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="cateCont">
+                <div></div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            value:'',
-        }
+    data() {
+        return {
+        value: ""
+        };
     }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
-body{
-    margin: 0;
-    padding: 0;
+* {
+  margin: 0;
+  padding: 0;
 }
-.search{
-    width: 100%;
-    .mint-searchbar{
-        background-color: #58bc58;
+div{
+    overflow: hidden;
+    .search {
+        width: 100%;
+        height: 53px;
+        position:fixed;
+        .mint-search {
+            height: 100%;
+            .mint-searchbar {
+                background-color: #31c27c;
+                height: 53px;
+                padding: 8px 15px;
+                .mint-searchbar-inner {
+                    width: 50%;
+                    height: 26px;
+                    border-radius: 8px;
+                }
+            }
+        }
+    }
+    .catesNav{
+        width: 100%;
+        margin-top:53px ;
+        // height: 615px;
+        .cate-list{
+            float: left;
+            width: 35%;
+            overflow-y: auto;
+            background-color: #fafafa;
+            // height: 100%;
+            li{
+                width: 100%;
+                text-align: left;
+                line-height: 60px;
+                display: block;
+                color: #323232;
+                height: 60px;
+                background: url(../img/classilyimg/li-bg.jpg) no-repeat;
+                background-size: auto 100%;
+                text-indent: 28px;
+                font-size: 14px;
+            }
+        }
+        .cateCont{
+            float: left;
+            width: 65%;
+            background-color: #be1966;
+            // height: 100%;
+        }
     }
 }
+
 </style>
